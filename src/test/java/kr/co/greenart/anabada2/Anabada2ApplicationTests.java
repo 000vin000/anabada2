165a412d0a3f920ccbdf2a9493c6dec032ac8401
RI4RU <dlw0603@gmail.com>
2025-03-14 15:12:55 +0900

    recreate project

---
+++ src/test/java/kr/co/greenart/anabada2/Anabada2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.greenart.anabada2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Anabada2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
