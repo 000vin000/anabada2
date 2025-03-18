c7bb19de48e60473f28223e336f5100d1572199c
hj-rxl <rubyxluna@gmail.com>
2025-03-18 15:05:51 +0900

    item entity 수정

--- src/main/java/kr/co/anabada/item/entity/Item.java
+++ src/main/java/kr/co/anabada/item/entity/Item.java
@@ -4,24 +4,21 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import kr.co.anabada.user.entity.Seller;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -30,23 +27,23 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Item {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "itemNo")
 	private Integer itemNo;
 	
-	@ManyToOne
-	@JoinColumn(name = "fk_sellerNo")
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "sellerNo", nullable = false)
 	private Seller sellerNo;
 	
-	@OneToOne
-	@JoinColumn(name = "fk_categoryNo", nullable = false)
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "categoryNo", nullable = false)
 	private Item_Category categoryNo;
 	
 	@Enumerated(EnumType.STRING)
 	@Column(name = "itemSaleType", nullable = false)
-	private ItemSaleType itemSaleType = ItemSaleType.auction;
+	private ItemSaleType itemSaleType = ItemSaleType.AUCTION;
 	
 	@Column(name = "itemTitle", length = 50, nullable = false)
 	private String itemTitle;
@@ -56,16 +53,17 @@ public class Item {
 	
 	@Enumerated(EnumType.STRING)
 	@Column(name = "itemStatus", nullable = false)
-	private ItemStatus itemStatus = ItemStatus.active;
-	
+	private ItemStatus itemStatus = ItemStatus.ACTIVE;
+
+	@Enumerated(EnumType.STRING)
 	@Column(name = "itemQuality")
-	private Integer itemQuality;
+	private ItemQuality itemQuality;
 	
 	@Column(name = "itemQuantity", nullable = false)
-	private Integer itemQuantity;
+	private Integer itemQuantity = 1;
 	
-	@Column(name = "itemPrice")
-	private Integer itemPrice;
+	@Column(name = "itemPrice", nullable = false)
+	private Long itemPrice = 0L;
 	
 	@Column(name = "itemLatitude", nullable = false)
 	private Double itemLatitude; // 위도
@@ -73,40 +71,55 @@ public class Item {
 	@Column(name = "itemLongitude", nullable = false)
 	private Double itemLongitude; // 경도
 	
-	@CreationTimestamp
-	@Column(name = "itemCreateDate")
-	private LocalDateTime itemCreatedDate;
-	
 	@Column(name = "itemStartDate")
 	private LocalDateTime itemStartDate;
 	
 	@Column(name = "itemEndDate")
 	private LocalDateTime itemEndDate;
 	
-	@UpdateTimestamp
-	private LocalDateTime itemUpdatedDate;
-	
 	@Column(name = "itemViewCnt", nullable = false)
-	private Integer itemViewCnt; // 조회수
+	private Integer itemViewCnt = 0; // 조회수
 	
 	@Column(name = "itemAvgRating")
 	private Double itemAvgRating; // 평균 평점
 	
+	@CreationTimestamp
+	@Column(name = "itemCreateDate", nullable = false, updatable = false)
+	private LocalDateTime itemCreatedDate;
+	
+	@UpdateTimestamp
+	@Column(name = "itemUpdatedDate", nullable = false)
+	private LocalDateTime itemUpdatedDate;
+	
+	
+	
 	public enum ItemSaleType {
-		auction, shop, exchange, donation
+		AUCTION,
+		SHOP,
+		EXCHANGE,
+		DONATION
 	}
 	
 	public enum ItemStatus {
-		active, expired, sold
+		ACTIVE,
+		EXPIRED,
+		SOLD
+	}
+	
+	public enum ItemQuality {
+		LOW,
+		MEDIUM,
+		HIGH
 	}
 	
 	// 기본값 설정
 	@PrePersist
 	public void prePersist() {
-		if (itemQuantity == null) { itemQuality = 1; }
-		if (itemPrice == null) { itemPrice = 0; }
-		if (itemViewCnt == null) { itemViewCnt = 0; }
-		if (itemStartDate == null) { itemStartDate = itemCreatedDate; }
-		if (itemEndDate == null) { itemEndDate = itemStartDate.plusDays(3); }
+		if (itemStartDate == null) {
+			itemStartDate = itemCreatedDate;
+		}
+		if (itemEndDate == null) {
+			itemEndDate = itemStartDate.plusDays(3);
+		}
 	}
 }
