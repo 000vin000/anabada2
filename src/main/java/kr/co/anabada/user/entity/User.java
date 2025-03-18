925eb0275ab51e8fa8f425477def1b6d07b0b7e1
hj-rxl <rubyxluna@gmail.com>
2025-03-18 14:11:23 +0900

    user entity 수정

--- src/main/java/kr/co/anabada/user/entity/User.java
+++ src/main/java/kr/co/anabada/user/entity/User.java
@@ -1,5 +1,10 @@
 package kr.co.anabada.user.entity;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -21,14 +26,14 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "userNo", nullable = false)
+    @Column(name = "userNo")
     private Integer userNo;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "userType", nullable = false)
     private UserType userType = UserType.INDIVISUAL;
 
-    @Column(name = "businessRegNo", length = 20)
+    @Column(name = "businessRegNo", length = 20, unique = true)
     private String businessRegNo;
 
     @Column(name = "userName", nullable = false, length = 20)
@@ -40,26 +45,43 @@ public class User {
     @Column(name = "userId", nullable = false, length = 20, unique = true)
     private String userId;
 
-    @Column(name = "userPw", nullable = false, length = 20)
+    @Column(name = "userPw", nullable = false, length = 100)
     private String userPw;
 
     @Column(name = "userPhone", nullable = false, length = 20, unique = true)
     private String userPhone;
 
-    @Column(name = "userEmail", nullable = false, length = 255, unique = true)
+    @Column(name = "userEmail", nullable = false, length = 100, unique = true)
     private String userEmail;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(name = "userStatus", nullable = false)
+    private UserStatus userStatus = UserStatus.ACTIVE;
 
     @Column(name = "userWarnCnt", nullable = false)
     private Byte userWarnCnt = 0;
 
-    @Column(name = "userCoin", nullable = false)
-    private Long userCoin = 0L;
+    @CreationTimestamp
+    @Column(name = "userCreatedDate", nullable = false, updatable = false)
+    private LocalDateTime userCreatedDate;
 
+    @UpdateTimestamp
+    @Column(name = "userUpdatedDate", nullable = false)
+    private LocalDateTime userUpdatedDate;
+
+    @Column(name = "userWithdrawalDate")
+    private LocalDateTime userWithdrawalDate;
+    
+    
     
     public enum UserType {
         INDIVISUAL,
-        ADMIN,
         BRAND
+    }
+    
+    public enum UserStatus {
+    	ACTIVE,
+    	INACTIVE
     }
 }
 
