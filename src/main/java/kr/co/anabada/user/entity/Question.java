0b2dc7d85fac7971141a004976cbdf1f9ad969b0
000vin000 <00vin0vin0vin00@naver.com>
2025-03-19 09:44:29 +0900

    question entity 수정

---
+++ src/main/java/kr/co/anabada/user/entity/Question.java
@@ -0,0 +1,56 @@
+package kr.co.anabada.user.entity;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Question")
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "questionNo", nullable = false)
+    private Integer questionNo;
+
+    @ManyToOne
+    @JoinColumn(name = "senderNo", nullable = false)  
+    private User sender; 
+
+    @Column(name = "qIsPrivate", nullable = false)
+    private Boolean qIsPrivate = false;
+
+    @Column(name = "questionTitle", nullable = false, length = 255) 
+    private String questionTitle;
+
+    @Column(name = "questionContent", nullable = false)
+    private String questionContent;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "questionStatus", nullable = false)
+    private QuestionStatus questionStatus = QuestionStatus.WAITING; 
+
+    @Column(name = "qCreatedDate", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP") 
+    private Timestamp qCreatedDate;
+
+    public enum QuestionStatus {
+        WAITING,
+        ANSWERED,
+        CLOSED
+    }
+}
