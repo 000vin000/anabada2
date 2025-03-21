fc1a866d2ce26d29df02f27b03d8253c4a0e5036
000vin000 <00vin0vin0vin00@naver.com>
2025-03-21 11:49:33 +0900

    ChatRoomRepository 수정

--- src/main/java/kr/co/anabada/chat/repository/ChatRoomRepository.java
+++ src/main/java/kr/co/anabada/chat/repository/ChatRoomRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import kr.co.anabada.chat.entity.Chat_Room;
 
 public interface ChatRoomRepository extends JpaRepository<Chat_Room, Integer> {
-    Optional<Chat_Room> findByItemItemNoAndUser1UserNoAndUser2UserNo(Integer itemNo, Integer user1No, Integer user2No);
+	Optional<Chat_Room> findByItemItemNoAndBuyerUserNoAndSellerUserNo(Integer itemNo, Integer buyerNo, Integer sellerNo);
 }
