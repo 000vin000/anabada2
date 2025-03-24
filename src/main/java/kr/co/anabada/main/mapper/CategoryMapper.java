2319c13d8d1bd3a6cadd65a6edb4d74adc0fb381
syeon <oks456@naver.com>
2025-03-24 09:34:28 +0900

    메인페이지 오류 수정

--- src/main/java/kr/co/anabada/main/mapper/CategoryMapper.java
+++ src/main/java/kr/co/anabada/main/mapper/CategoryMapper.java
@@ -5,6 +5,9 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface CategoryMapper {
-	@Select("SELECT category_name FROM item_category WHERE category_no LIKE CONCAT('%', #{gender}, '%')")
+	@Select("SELECT category_name FROM item_category WHERE category_no LIKE CONCAT('__', #{gender}, '0000')")
 	String getGenderName(String gender);
+
+	@Select("SELECT category_name FROM item_category WHERE category_no LIKE CONCAT('%', #{cateDetail}) LIMIT 1")
+	String findCateDetail(String cateDetail);
 }
