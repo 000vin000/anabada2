4cadc10df59aade6630b710cff2e9d5dcd2285d3
hj-rxl <rubyxluna@gmail.com>
2025-03-28 15:58:32 +0900

    ItemStatusScheduler 구현 - WAITING => ACTIVE - ACTIVE => RESERVED - ACTIVE => SOLD

--- src/main/java/kr/co/anabada/item/repository/ItemRepository.java
+++ src/main/java/kr/co/anabada/item/repository/ItemRepository.java
@@ -3,7 +3,10 @@ package kr.co.anabada.item.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import kr.co.anabada.item.entity.Item;
 import kr.co.anabada.user.entity.Seller;
@@ -13,5 +16,31 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
 
 	List<Item> findBySeller(Seller seller);
 
-	int countBySeller_SellerNo(Integer sellerNo); //userProfile
+	int countBySeller_SellerNo(Integer sellerNo); // userProfile
+
+	@Query("SELECT itemNo FROM Item")
+	List<Integer> findAllItemNos(); // ItemStatusScheduler
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE Item SET itemStatus = ItemStatus.ACTIVE " +
+			"WHERE itemStatus = ItemStatus.WAITING " +
+			"AND itemSaleStartDate <= now()")
+	int activateWaitingItems(); //ItemStatusScheduler
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE Item i SET i.itemStatus = ItemStatus.EXPIRED " +
+			"WHERE i.itemStatus = ItemStatus.ACTIVE " +
+			"AND i.itemSaleEndDate <= now() " +
+			"AND NOT EXISTS (SELECT b FROM Bid b WHERE b.item = i)")
+	int expireActiveItems(); //ItemStatusScheduler
+	
+	@Modifying
+	@Transactional
+	@Query("UPDATE Item i SET i.itemStatus = ItemStatus.RESERVED " +
+			"WHERE i.itemStatus = ItemStatus.ACTIVE " +
+			"AND i.itemSaleEndDate <= now() " +
+			"AND EXISTS (SELECT b FROM Bid b WHERE b.item = i)")
+	int reserveActiveItems(); //ItemStatusScheduler
 }
